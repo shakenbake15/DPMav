fd2413c007410bf574c2f9c3da0d9f9192856538
shakenbake15 <132719763+shakenbake15@users.noreply.github.com>
2023-05-10 19:09:40 -0500

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -284,7 +284,7 @@ class CarInterfaceBase(ABC):
   def apply(self, c: car.CarControl, now_nanos: int) -> Tuple[car.CarControl.Actuators, List[bytes]]:
     pass
 
-  def create_common_events(self, cs_out, extra_gears=(GearShifter.low,GearShifter.eco), pcm_enable=True, allow_enable=True,
+  def create_common_events(self, cs_out, extra_gears=(GearShifter.brake,GearShifter.sport), pcm_enable=True, allow_enable=True,
                            enable_buttons=(ButtonType.accelCruise,ButtonType.decelCruise)):
     events = Events()
 
